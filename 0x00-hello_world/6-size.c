a05ae5330b3854b39cb2ef227ad264889ddb3895
natnael gezahegn <natigezu15@gmail.com>
2022-06-30 01:06:42 -0700

    my first c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int intType;
+float floatType;	 
+char charType;
+long int longinttype;
+long long int longlongintType;
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longinttype));
+printf("Size of a long long: %zu byte(s)\n", sizeof(longlongintType));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatint));
+return 0;
