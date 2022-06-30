f9a0c12d6e332f966ec46b82e97fbb0cf3f22018
natnael gezahegn <natigezu15@gmail.com>
2022-06-30 01:15:10 -0700

    my first c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,14 +3,17 @@
  * main - main block
  * Return: 0
  */
+{
 int intType;
 float floatType;	 
 char charType;
 long int longinttype;
 long long int longlongintType;
-printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-printf("Size of a long int: %zu byte(s)\n", sizeof(longinttype));
-printf("Size of a long long: %zu byte(s)\n", sizeof(longlongintType));
-printf("Size of a float: %zu byte(s)\n", sizeof(floatint));
+printf("Size of a char: %d byte(s)\n", sizeof(charType));
+printf("Size of an int: %d byte(s)\n", sizeof(intType));
+printf("Size of a long int: %d byte(s)\n", sizeof(longinttype));
+printf("Size of a long long: %d byte(s)\n", sizeof(longlongintType));
+printf("Size of a float: %d byte(s)\n", sizeof(floatint));
 return 0;
+
+}
