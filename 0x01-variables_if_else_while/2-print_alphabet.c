42004664eaa803aeb8a23dcde290aa31d02963d0
Netgezu <106767573+Netgezu@users.noreply.github.com>
2022-07-01 14:17:40 +0300

    Update 2-print_alphabet.c
    correction

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+/**
+* main - main block
+* Return: o
+*/
 int main(void)
 {
 char ch;
-for(ch = 'a'; ch <='z'; ch++)
+for(ch = 'a'; ch <= 'z'; ch++)
 putchar(ch);
 putchar('\n');
 return (0);
