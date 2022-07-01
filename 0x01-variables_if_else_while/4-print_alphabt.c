cf402337668a46e818e34b825cdd5eb892769220
natnael gezahegn <natigezu15@gmail.com>
2022-07-01 04:35:46 -0700

    except

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - main block
+* Return: 0
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+if (ch != 'e' && ch != 'q')
+putchar(ch);
+else
+putchar('\n');
+return (0);
+}
