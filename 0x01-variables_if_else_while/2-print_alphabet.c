1e23e8b87706c04ef84916e4913c03cc7fd0e9bd
Netgezu <106767573+Netgezu@users.noreply.github.com>
2022-07-02 10:05:47 +0300

    again

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 /**
-<<<<<<< HEAD
- *main - main block
- *Return: 0
- */
-=======
 * main - main block
 * Return: o
 */
->>>>>>> d2ff178a50b839beadd1521f480a95d30b156ab6
 int main(void)
 {
 char ch;
