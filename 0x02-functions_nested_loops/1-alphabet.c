ca185747362b1ff6b5790524109fe63622d231d3
Netgezu <106767573+Netgezu@users.noreply.github.com>
2022-07-05 14:59:30 +0300

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* main - check the code
+* print_alphabet - check the code
 * Return: Always 0
 */
 void print_alphabet(void)
@@ -9,6 +9,6 @@ char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 {
 _putchar(ch);
-_putchar('\n');
 }
+_putchar('\n');
 }
