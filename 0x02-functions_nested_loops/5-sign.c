93c3780a60ba315eb9d77e4d14a968ad8a8efb07
Netgezu <106767573+Netgezu@users.noreply.github.com>
2022-07-05 15:53:02 +0300

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,42 +1,17 @@
-#include <stdio.h>
-#include <ctype.h>
+#include "main.h"
 /**
-* main - check the code.
+* print_sign - check the code.
+* @c: is ascii
 * Return: Always 0.
 */
-int main(void)
-{
-int r;
-r = print_sign(98);
-putchar(',');
-putchar(' ');
-putchar(r + '0');
-putchar('\n');
-r = print_sign(0);
-putchar(',');
-putchar(' ');
-putchar(r + '0');
-putchar('\n');
-r = print_sign(0xff);
-putchar(',');
-putchar(' ');
-putchar(r + '0');
-putchar('\n');
-r = print_sign(-1);
-putchar(',');
-putchar(' ');
-putchar(r + '0');
-putchar('\n');
-return (0);
-}
 int print_sign(int n)
 {
-if(n > 0)
+if (n > 0)
 {
 putchar('+');
 return (1);
 }
-else if(n == 0)
+else if (n == 0)
 {
 putchar('0');
 return (0);
