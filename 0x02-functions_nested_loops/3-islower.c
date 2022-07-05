c3587afd486d9875b3d0523fe53f14ba14ff979e
Netgezu <106767573+Netgezu@users.noreply.github.com>
2022-07-05 15:27:12 +0300

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,20 +6,10 @@
 */
 int _islower(int c)
 {
+c = 100;
 if (islower(c))
 return (1);
 else
 return (0);
 }
-int nati(void)
-{
-int r;
-r = _islower('H');
-_putchar(r + '0');
-r = _islower('o');
-_putchar(r + '0');
-r = _islower(108);
-_putchar(r + '0');
-_putchar('\n');
-return (0);  
-}
+
