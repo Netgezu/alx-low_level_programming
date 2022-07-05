07fc8ba8e305ac23db65242006b177df531cb8e9
natnael gezahegn <natigezu15@gmail.com>
2022-07-05 11:00:11 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * times_table - prints the time table
+ * Return: always 0.
+ */
+void times_table(void)
+{
+int x, y, product;
+for (x = 0; x <=9; x++)
+{
+for (y = 0; y <= 9; y++)
+{
+product = X * y;
+if (y != 0)
+{
+_putchar(',');
+_putchar(' ');
+}
+else if (y == 0)
+{
+_putchar('0');
+}
+else if (product >= 10)
+{
+_putchar((product / 10) + '0');
+_putchar((product % 10) + '0');
+}
+else if ((product < 10) && (y != 0))
+{
+_putchar(' ');
+_putchar((product % 10) + '0');
+}
+}
+_putchar('\n');
+}
+}
