02c9e4de65e790dd99a548841b7a991272af4498
Netgezu <106767573+Netgezu@users.noreply.github.com>
2022-07-05 13:39:16 +0300

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 /**
 * main - check the code
@@ -5,7 +6,7 @@
 */
 int main(void)
 {
-print_alphabet(void);
+print_alphabet();
 return (0);
 }
 void print_alphabet(void)
