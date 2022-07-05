4b2d796f25cbf2fdd3bd589cffe49840226e779d
natnael gezahegn <natigezu15@gmail.com>
2022-07-05 01:07:00 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+* main - check the code.
+* Return: Always 0.
+*/
+int main(void)
+{
+int r;
+r = _isalpha('H');
+putchar(r + '0');
+r = _isalpha('o');
+putchar(r + '0');
+r = _isalpha(108);
+putchar(r + '0');
+r = _isalpha(';');
+putchar(r + '0');
+putchar('\n');
+return (0);
+}
+int _isalpha(int c)
+{
+if (isalpha(c))
+return (1);
+else
+return (0);
+}
