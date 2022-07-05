562c5a989248608b3a41302d9786da8ded7a5ad8
Netgezu <106767573+Netgezu@users.noreply.github.com>
2022-07-05 15:12:02 +0300

    x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,9 +7,9 @@ void print_alphabet_x10(void)
 {
 int i;
 char ch;
-for(i = 0; i <= 9; i++)
+for (i = 0; i <= 9; i++)
 {
-for(ch = 'a'; ch <= 'z'; ch++)
+for (ch = 'a'; ch <= 'z'; ch++)
 {
 putchar(ch);
 }
