646b02465b8b6da24f1333cf67875478ad66eac1
Netgezu <106767573+Netgezu@users.noreply.github.com>
2022-07-07 21:33:46 +0300

    more

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,11 +5,11 @@
 */
 void more_numbers(void)
 {
-int n, m;
-for (n = 0; n < 10; n++)
+ int n, m;
+ for (n = 0; n < 10; n++)
 {
-for (m = 48; m < 58; m++)
-_putchar(m);
+ for (m = 48; m < 58; m++)
+ _putchar(m);
 }
-_putchar('\n');
+ _putchar('\n');
 }
