000025d2d8a1fee585f2d3bf6bf401a078fdd6b5
Netgezu <106767573+Netgezu@users.noreply.github.com>
2022-07-07 16:31:39 +0300

    digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* _isdigit - checks c is digit.
-* @c: is number
+* _isdigit - check the code.
+* @c: is ascii
 * Return: Always 0.
 */
-int _isdigit(int c)
+int _isupper(int c)
 {
 if (c >= 0 && c <= 9)
 return (1);
