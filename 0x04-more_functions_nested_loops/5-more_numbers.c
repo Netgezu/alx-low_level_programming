cb8a67e717100a45c271438e5225c7dda226b8a6
Netgezu <106767573+Netgezu@users.noreply.github.com>
2022-07-07 21:32:08 +0300

    more

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_more_numbers - check the code
+* Return: Always 0
+*/
+void print_more_numbers(void)
+{
+int n, m;
+for (n = 0; n < 10; n++)
+{
+for (m = 48; m < 58; m++)
+_putchar(m);
+}
+_putchar('\n');
+}
