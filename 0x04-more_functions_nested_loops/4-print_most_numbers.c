4e88354be9ddde8f49ea9850781ce7785fc0c0c7
Netgezu <106767573+Netgezu@users.noreply.github.com>
2022-07-07 21:20:26 +0300

    most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
- * print_most_numbers - prints numbers except 2 and 4
- * Return: returns 0
- */
-void print_most_numbers(void)
+* print_most_numbers - check the code
+* Return: Always 0
+*/
+void print_numbers(void)
 {
-for (i = 48; i < 58; i++)
+int n;
+for (n = 48; n < 58; n++)
 {
-if (i != 50 && i != 52)
-_putchar(i);
+if (n != 50 && n != 52)
+_putchar(n);
 }
 _putchar('\n');
 }
