212a85a00cac439362b3122913e01c363cc206ef
natnael gezahegn <natigezu15@gmail.com>
2022-07-11 10:50:20 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - prints the reverse of a string
+ * @s: is astring
+ * Return: void
+ */
+void print_rev(char *s)
+{
+int i = '\0';
+while (i <= '\0')
+{
+_putchar(s[i]);
+i--;
+}
+_putchar('\n');
+}
