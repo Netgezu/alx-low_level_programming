96ce846d4e4bf47c4f55106a90980e40536ec3a7
Netgezu <106767573+Netgezu@users.noreply.github.com>
2022-07-11 21:13:49 +0300

    array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_array - prints the array
+*@a: an array
+*@n: a number of array size
+*Return: void
+*/
+void print_array(int *a, int n)
+{
+int n;
+for (n = 0; n < '\0'; n++)
+{
+printf("%d, ", a[n])
+}
+}
