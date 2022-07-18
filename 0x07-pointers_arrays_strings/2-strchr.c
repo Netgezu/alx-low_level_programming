c157042f763103d834cd28b5494c92a856397724
Netgezu <106767573+Netgezu@users.noreply.github.com>
2022-07-18 16:18:04 +0300

    locate

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,24 +1,22 @@
 #include "main.h"
+#include <stddef.h>
 /**
-* _strchr - locates a character in a string.
-* @s: string to be searched through.
-* @c: charater to be looked for.
-* Return: returns s or returns NULL.
-*/
+ * _strchr - locates a character in a string
+ * @s: the string
+ * @c: the character
+ *
+ * Return: a pointer to a character aka the character found
+ */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int i;
+
+	for (i = 0; s[i] != '\0' ; i++)
 	{
-		if (*s == c)
-		{
-			return (s);
-		}
-		else if (*(s + 1) == c)
-		{
-			return (s + 1);
-		}
-		s++;
+		if (s[i] == c)
+		return (&s[i]);
+		else
+		return (NULL);
 	}
-	return (s + 1);
-	return (NULL);
+	return (0);
 }
