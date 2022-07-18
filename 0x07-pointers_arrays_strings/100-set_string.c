1a2959eba2f42732aa76b64f9453c7f0e480fee3
Netgezu <106767573+Netgezu@users.noreply.github.com>
2022-07-18 17:09:21 +0300

    set

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -3,10 +3,9 @@
 * set_string - function that sets the value of a pointer to a char
 * @s:  pointer to the adderss of string s
 * @to: string to set value to
-*
 * Return: nothing
- */
+*/
 void set_string(char **s, char *to)
 {
-	*s = *to;
+	*s = to;
 }
