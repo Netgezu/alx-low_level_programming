31dfbe3cf4da5e00011d51ed7cc966752919a04c
Netgezu <106767573+Netgezu@users.noreply.github.com>
2022-07-18 16:16:02 +0300

    locate

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
 * _strchr - locates a character in a string.
-* Description: returns a point to the first occurance of the char c.
 * @s: string to be searched through.
 * @c: charater to be looked for.
 * Return: returns s or returns NULL.
