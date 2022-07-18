5968cfeaf51d6cfdc5ad148eec2d133baff76b2a
Netgezu <106767573+Netgezu@users.noreply.github.com>
2022-07-18 16:02:04 +0300

    copy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,12 +8,15 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-int i = 0;
-while (n > 0)
-{
-dest = src;
-i++;
-n--;
-}
-return (dest);
+	int i = 0;
+	int j = 0;
+
+	while (n > 0)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+		n--;
+	}
+	return (dest);
 }
