2dd50559642c5668499501a0dde4de37b1e4d5f1
Netgezu <106767573+Netgezu@users.noreply.github.com>
2022-07-25 16:31:06 +0300

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,9 +11,9 @@ char *str_concat(char *s1, char *s2)
 	char *strDup;
 	int i, j;
 	if (s1 == NULL)
-		s1 = " ";
+		s1 = "";
 	if (s2 == NULL)
-		s2 = " ";
+		s2 = "";
 	i = j = 0;
 	while (s1[i] != '\0')
 		i++;
