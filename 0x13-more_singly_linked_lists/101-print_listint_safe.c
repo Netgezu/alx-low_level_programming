d32bf5cb83ac9217653036d55bbde258192d2717
Netgezu <106767573+Netgezu@users.noreply.github.com>
2022-08-16 17:14:25 +0300

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,60 +1,30 @@
 #include "lists.h"
 
 /**
- * free_aux - frees a linked list
- * @p: head of a list.
- *
- * Return: no return.
- */
-void free_aux(aux_list *p)
-{
-	aux_list *aux;
-
-	if (p == NULL)
-	{
-		return;
-	}
-	while (p != NULL)
-	{
-		aux = p;
-		p = p->next;
-		free(aux);
-	}
-}
-
-/**
- * print_listint_safe - prints a linked list.
- * @head: head of a list.
- *
- * Return: number of nodes in the list.
+ * print_listint_safe - prints list, avoids loop
+ * @head: points to start of list
+ * Return: number of nodes in size_t or exit 98
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t c = 0;
-	aux_list *a, *b, *t;
-
-	a = NULL;
-	for (; head != NULL; c++, head = head->next)
+	/* declarations */
+	size_t nodeCount = 0;
+	/* check for null pointer */
+	while (head)
 	{
-		t = malloc(sizeof(aux_list));
-		if (!t)
-			exit(98);
-		t->p = (void *)head;
-		t->next = a;
-		a = t;
-		b = a->next;
-		while (b != NULL)
-		{
-			if (head == b->p)
-			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
-				free_aux(a);
-				return (c);
-			}
-			b = b->next;
-		}
 		printf("[%p] %d\n", (void *)head, head->n);
+		nodeCount += 1;
+
+		if (head > head->next)
+		{
+			head = head->next;
+		}
+		else
+		{
+			head = head->next;
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			break;
+		}
 	}
-	free_aux(a);
-	return (c);
+	return (nodeCount);
 }
