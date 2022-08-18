a438a154bfcc0d1b769894c851b3494c35a1a75a
Netgezu <106767573+Netgezu@users.noreply.github.com>
2022-08-18 16:57:45 +0300

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,20 +1,33 @@
 #include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 /**
- * binary_to_uint - binary to integer.
- * @b: binary.
- * Return: number.
+ *binary_to_uint - converts a binary number to an unsigned int
+ *@b:pointer to a string containing 1 and 0
+ *Return:converted number or 0,otherwise
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int bit = 0;
-while (b && *b)
-{
-	if (*b != '0' && *b != '1')
-		return (0);
+	int i;
+	unsigned int n = 0;
 
-	bit = bit << 1;
-	bit = bit | (*b - '0');
-	b++;
-}
-	return (bit);
+	if (b == NULL)
+		return (0);
+	i = 0;
+	while (b[i])
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		i++;
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		n = n << 1;
+		if (b[i] == '1')
+		{
+			n += 1;
+		}
+	}
+	return (n);
 }
